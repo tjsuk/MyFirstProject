5fe82c2078f05778c86bc1410740481ca4dbc1ec
tjsuk <156339246+tjsuk@users.noreply.github.com>
2026-03-18 15:15:11 +0000

    Added examples of variables and demonstrated Strings, Wrapper Classes and using reference types.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,88 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+void main() {
+        // System.out.println("Hello, World!");
+
+        byte age = 21;
+        byte maxValue = Byte.MAX_VALUE;
+
+        //System.out.println(maxValue);
+
+        maxValue = (byte)(maxValue + 1);
+
+        //System.out.println(maxValue);
+
+        /*
+        short sAge = age;
+        int iAge = sAge;
+        long lAge = iAge;
+
+        lAge = Long.MAX_VALUE;
+
+        iAge = (int)lAge;
+        System.out.println(iAge);
+
+        byte vAge = (byte)16;
+
+        int num = 5;
+        Integer iNum = num; // Autoboxing
+
+        Integer jNum = 54;
+        int numI = jNum;  // unboxing
+
+        int x;
+
+        double pi = 3.14159265358979323846264338;
+        System.out.println(pi);
+
+        double pi2 = 3.14159265358979323846264338f;
+        System.out.println(pi2);
+
+        char letter = 'G';
+
+
+        String name1 = "Fred ";  // First one
+        String name2 = "Fred";
+        String name3 = "Fred";
+        String name4 = "Fred";
+        String name5 = "Fr" + "ed";
+        String name6 = new String("Fred");
+
+        System.out.println(name1 == name6);  // checks the reference
+        System.out.println(name1.equals(name6));  // checks the value
+
+        name6 = "Barney";
+        name1 = "Barney";
+
+        System.out.println(name1 == name6);
+
+
+
+        String firstName = "Alice";
+        String lastName = "Smith";
+
+        IO.println(firstName + " " + lastName);
+
+        // String output = firstName + " " + lastName;
+        String output = String.format("%s %s", firstName, lastName);
+        IO.println(output);
+
+        IO.println(firstName.toLowerCase());
+        IO.println(lastName.toUpperCase());
+
+        String breed = "GoldenRetriever";
+        String firstBreed = breed.substring(0,6);
+        String lastBreed = breed.substring(6);
+
+        IO.println(firstBreed);
+        IO.println(lastBreed);
+
+        byte primitiveNum = 5; // sits in the stack
+        Byte wrapperNum = 5;  // oodles of memory
+
+        Character bigNumber = null;
+
+        Integer a = -129;
+        Integer b = -129;
+
+        IO.println(a.equals(b));
+        */
 }
